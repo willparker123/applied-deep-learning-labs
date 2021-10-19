95e691fe8510d62f2c8c19deefb44716879f0a70
Will Parker <gg18045@bc4login3.bc4.acrc.priv>
2021-10-19 14:50:41 +0100

    Finished lab 1; Pytorch/Numpy, MLP Network training, accuracy and forward-backwards pass of backpropagation

--- lab-1/train.sh
+++ lab-1/train.sh
@@ -2,8 +2,9 @@
 srun \
     --partition gpu \
     --gres gpu:1 \
-    #-A comsm0045 \
-    #--reservation comsm0045-lab1 \
     --mem 64GB \
     -t 0-00:15 \
     python train_mnist.py
+
+#-A comsm0045 \
+#--reservation comsm0045-lab1 \
